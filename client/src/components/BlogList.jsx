2a8933a9ea3462e3297b5444680044bf1c70ef22
akshaychavan23031998 <akshayrchavan07@gmail.com>
2025-08-24 21:38:42 +0530

    Completed BlogCard.

--- client/src/components/BlogList.jsx
+++ client/src/components/BlogList.jsx
@@ -1,7 +1,8 @@
 import React from "react";
-import { blogCategories } from "../assets/assets";
+import { blog_data, blogCategories } from "../assets/assets";
 import { useState } from "react";
 import { motion } from "motion/react";
+import BlogCard from "./BlogCard";
 
 const BlogList = () => {
   const [menu, setMenu] = useState("All");
@@ -28,7 +29,14 @@ const BlogList = () => {
           </div>
         ))}
       </div>
-      <div>{/*--- blog cards ---*/}</div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-8 mb-24 mx-8 sm:mx-16 xl:mx-40">
+        {/*--- blog cards ---*/}
+        {blog_data
+          .filter((blog) => (menu === "All" ? true : blog.category === menu))
+          .map((blog) => (
+            <BlogCard key={blog._id} blog={blog} />
+          ))}
+      </div>
     </div>
   );
 };
