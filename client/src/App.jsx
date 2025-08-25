8d7d2b845c662b7cc5ad5a2bbd063275c70c828a
akshaychavan23031998 <akshayrchavan07@gmail.com>
2025-08-25 21:21:28 +0530

    Completed the Admin Login page.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import Dashboard from "./pages/admin/Dashboard";
 import AddBlog from "./pages/admin/AddBlog";
 import ListBlog from "./pages/admin/ListBlog";
 import Comments from "./pages/admin/Comments";
+import Login from "./components/admin/Login";
 
 const App = () => {
   return (
@@ -14,8 +15,9 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/blog/:id" element={<Blog />} />
-        <Route path="/admin" element={<Layout />}>
-          <Route index element={<Dashboard />} />     {/* this line also writen as ==>>  <Route path="" element={<Dashboard />} />  */}
+        <Route path="/admin" element={true ? <Layout /> : <Login />}>
+          <Route index element={<Dashboard />} />{" "}
+          {/* this line also writen as ==>>  <Route path="" element={<Dashboard />} />  */}
           <Route path="addBlog" element={<AddBlog />} />
           <Route path="listBlog" element={<ListBlog />} />
           <Route path="comments" element={<Comments />} />
